117a0461d6a9e006ef6d41f9278752f97bac1aec
Jonathan Ikpeazu <jonathan@playvs.com>
2020-02-24 18:10:34 -0800

    remove view templating

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import express from "express";
-import path from "path";
 
 // initialize configuration
 dotenv.config();
@@ -11,14 +10,8 @@ const port = process.env.SERVER_PORT;
 
 const app = express();
 
-// Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
-
-// define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-  // render the index template
-  res.render( "index" );
+  res.json({ message: 'Hello, World!' });
 } );
 
 // start the express server
