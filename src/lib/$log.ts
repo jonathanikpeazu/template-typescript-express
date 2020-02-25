a5642577ab6a1dc22d3d7a4cf9b116b530dd4f63
Jonathan Ikpeazu <jonathan@playvs.com>
2020-02-25 14:35:41 -0800

    install babel typescript config

--- src/lib/$log.ts
+++ src/lib/$log.ts
@@ -2,6 +2,6 @@ import bunyan from 'bunyan'
 
 export const $log = bunyan.createLogger({ name: 'core' })
 
-export function getLogger(name): bunyan.Logger {
+export function getLogger(name): bunyan {
   return bunyan.createLogger({ name })
 }
