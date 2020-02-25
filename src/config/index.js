8a3fd8bb29dc63e759e1b42060a25faa07788ebf
Jonathan Ikpeazu <jonathan@playvs.com>
2020-02-25 12:11:00 -0800

    create config package

---
+++ src/config/index.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
+const defaultConfig = require('./default');
+
+const envConfigs = {
+  development: require('./development'),
+  test: require('./test'),
+  production: require('./production'),
+  staging: require('./staging'),
+};
+
+function getEnvConfig(nodeEnv = process.env.NODE_ENV) {
+  if (_.isEmpty(nodeEnv)) {
+    return {};
+  }
+
+  const envConfig = envConfigs[nodeEnv];
+  if (!envConfig) {
+    throw new Error(`Unable to locate configuration file: Invalid NODE_ENV: ${nodeEnv}`);
+  }
+
+  return envConfig;
+}
+
+// Doing this in the weird mutative way so that we can get editor autocompletion.
+let config = defaultConfig;
+const envConfig = _(getEnvConfig())
+  .mapValues(v => (_.isUndefined(v) ? null : v))
+  .defaultsDeep(config)
+  .value();
+
+config = envConfig;
+
+module.exports = { config };
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
-import dotenv from 'dotenv';
 import express from 'express';
 
-// initialize configuration
-dotenv.config();
-
-// port is now available to the Node.js runtime
-// as if it were an environment variable
-const port = process.env.SERVER_PORT;
+import { config } from './config';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, World!' });
+  res.json({ message: 'Hello, World!' });
 });
 
-// start the express server
-app.listen(port, () => {
-    // tslint:disable-next-line:no-console
-    console.log(`server started at http://localhost:${port}`);
+app.listen(config.port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${config.port}`);
 });
