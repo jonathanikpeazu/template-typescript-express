d495172b1da6b29a8017428ec1f4f26f6cb10c3d
Jonathan Ikpeazu <jonathan@playvs.com>
2020-02-27 15:39:28 -0800

    graphql api base

---
+++ src/api/resolvers/index.ts
@@ -0,0 +1,17 @@
+type Book = { title: string; author: string }
+const books: Book[] = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+]
+
+export const resolvers = {
+  Query: {
+    books: (): Book[] => books,
+  },
+}
