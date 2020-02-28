7b55a23709c1c711b27053cacb918a1bd1cef1b3
Jonathan Ikpeazu <jonathan@playvs.com>
2020-02-28 13:01:15 -0800

    fix seed script

--- tools/sql_setup/run-seed.sh
+++ tools/sql_setup/run-seed.sh
@@ -2,6 +2,7 @@ THIS_ENV=$1
 SEED_FILENAME=$2
 THIS_ENV=${THIS_ENV:-development}
 
-NODE_ENV=${THIS_ENV} npx babel-node --extensions ".js,.ts" \
-"${BASH_SOURCE%/*}/../../node_modules/.bin/knex" seed:run --specific=${SEED_FILENAME} \
---knexfile "${BASH_SOURCE%/*}/knexfile.js"
+npx babel-node --extensions ".js,.ts" \
+"${BASH_SOURCE%/*}/../../node_modules/knex/bin/cli" \
+--knexfile "${BASH_SOURCE%/*}/knexfile.js" \
+seed:run --specific=${SEED_FILENAME}
