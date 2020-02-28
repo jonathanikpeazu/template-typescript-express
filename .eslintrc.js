ed0a029fd7f2db9f90582ff4c4ad7ae28f4efd83
Jonathan Ikpeazu <jonathan@playvs.com>
2020-02-28 13:14:56 -0800

    fix linting on js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
   extends: [
     'eslint-config-johno'
+  ],
+  overrides: [
+    {
+      files: './tools/sql_setup/migrations/*',
+      rules: {
+        '@typescript-eslint/no-explicit-any': 0
+      }
+    }
   ]
 }
