53297b0e595c1676561c81fb96c70cb558fe339b
Jonathan Ikpeazu <jonathan@playvs.com>
2020-02-28 16:56:57 -0800

    resolvers working

--- src/api/resolvers/index.ts
+++ src/api/resolvers/index.ts
@@ -1,17 +1,32 @@
 import { GraphQLDateTime } from 'graphql-iso-date'
 import { GraphQL } from '../graphql'
-import { Book } from '../../models'
+import { Author, Book } from '../../models'
 
 const queryResolvers: GraphQL.QueryResolvers = {
-  books: async () => {
-    const results = await Book.query()
-    return results
-  },
+  books: async () => Book.query(),
+  authors: async () => Author.query(),
+}
+
+const bookResolvers: GraphQL.BookResolvers = {
+  author: async ({ id }) =>
+    Author.query()
+      .whereExists(Author.relatedQuery('books').findById(id))
+      .first(),
+}
+
+const authorResolvers: GraphQL.AuthorResolvers = {
+  books: async ({ id }) => Book.query().whereExists(Book.relatedQuery('author').findById(id)),
 }
 
 export const resolvers = [
   {
     Query: queryResolvers,
+  },
+  {
+    Book: bookResolvers,
+  },
+  {
+    Author: authorResolvers,
   },
   {
     ISODateTime: GraphQLDateTime,
